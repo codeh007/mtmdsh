e9cddfba079b29b13f5a741a9ef262dcda44fb8a
a <a@a.com>
2026-08-23 16:46:45 +0000

    1

--- packages/plugin-hello/src/demo.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from "@deepseek-ai/cordis";
-import * as hello from "./index.js";
-
-const ctx = new Context();
-const fiber = ctx.plugin(hello);
-
-await fiber.await();
-await fiber.dispose();
